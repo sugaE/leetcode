5110223172575a9e5858b95bf4b4cb03a6dbe705
suga <suga_e@outlook.com>
2021-05-11 19:48:33 +0800

    update playground.cpp

--- playground.cpp
+++ playground.cpp
@@ -1,12 +1,14 @@
-// https://leetcode.com/problems/delete-operation-for-two-strings/
+// https://leetcode.com/problems/maximum-points-you-can-obtain-from-cards/
 #include <iostream>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
 class Solution
 {
 public:
-  int minDistance(string word)
+  int maxScore(const vector<int> &cardPoints, int k)
   {
     return 0;
   }
@@ -16,9 +18,16 @@ int main()
 {
 
   Solution solution;
+  vector<int> results;
 
-  auto res = solution.minDistance("lee");
-  cout << res << endl;
+  results.push_back(solution.maxScore({1, 20}, 2));
+  results.push_back(solution.maxScore({1, 1000, 1}, 1));
+  results.push_back(solution.maxScore({1, 79, 80, 1, 1, 1, 200, 1}, 3));
+
+  for (auto i : results)
+  {
+    cout << i << endl;
+  }
 
   return 0;
 }
